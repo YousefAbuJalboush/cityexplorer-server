aade2c06b7ebe1a81e9ffadc7610bcdc8948623a
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-16 14:54:38 +0300

    add Movies API

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 const cors = require('cors');
 
@@ -33,7 +34,7 @@ app.get('/weather', (req, res) => {
       const responseDataWeatherBit = response.data.data.map(objWeatherBit => new Weather(objWeatherBit));
       res.json(responseDataWeatherBit)
     }).catch(error => {
-      res.send('Something went wrong :' . error.message)
+      res.send('Something went wrong in reqWeatherBit :' + error.message)
     });
   } else {
     res.send('You did not provide the lat and lon')
@@ -46,6 +47,40 @@ class Weather {
   constructor(dataForWeather) {
     this.descriptionWeather = dataForWeather.weather.description;
     this.dateWeather = dataForWeather.valid_date;
+  }
+}
+
+
+app.get('/movies', (req, res) => {
+
+  const cityName = req.query.cityName;
+  if (cityName) {
+
+    const reqTheMovieDB = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`;
+    axios.get(reqTheMovieDB).then(response => {
+      const responseDataTheMovieDB = response.data.results.map( objTheMovieDB =>
+          new Movie(objTheMovieDB.title, objTheMovieDB.overview, objTheMovieDB.vote_average, objTheMovieDB.vote_count, `https://image.tmdb.org/t/p/w500${objTheMovieDB.poster_path}`, objTheMovieDB.popularity, objTheMovieDB.release_date)
+      );
+      responseDataTheMovieDB.length == 0 ? res.json("No movies result") : res.json(responseDataTheMovieDB);
+    }).catch(error => {
+      res.send('Something went wrong in reqTheMovieDB :' + error.message)
+    });
+  } else {
+    res.send('You did not provide the City Name')
+  }
+
+
+});
+
+class Movie {
+  constructor(title, overview, vote_average, vote_count, poster_path, popularity, release_date) {
+    this.title = title;
+    this.overview = overview;
+    this.vote_average = vote_average;
+    this.vote_count = vote_count;
+    this.poster_path = poster_path;
+    this.popularity = popularity;
+    this.release_date = release_date;
   }
 }
 
